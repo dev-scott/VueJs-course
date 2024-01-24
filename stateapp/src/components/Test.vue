93dd7ca0084b9ca1145e8f61dc5ce53bddec97ed
Dev-scott <sadoscott25@gmail.com>
2024-01-24 05:50:43 +0100

    store reusable logic with composables

--- stateapp/src/components/Test.vue
+++ stateapp/src/components/Test.vue
@@ -1,11 +1,15 @@
 <script setup>
 import useNumbers from "../composable/useNumbers";
 
-const { numbers } = useNumbers();
+const { numbers , addNumber } = useNumbers();
 </script>
 
 <template>
-  <div>it's test vue {{ numbers }}</div>
+  <div>
+    it's test vue {{ numbers }}
+
+    <button @click="addNumber(2)">{{ numbers }}</button>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- stateapp/src/composable/useNumbers.js
+++ stateapp/src/composable/useNumbers.js
@@ -3,11 +3,12 @@ import { ref } from "vue";
 export default function () {
   const numbers = ref([1, 2, 3, 4, 5]);
 
-    return {
-        numbers
-    }
+  const addNumber = (num) => {
+    numbers.value.push(num);
+  };
 
+  return {
+    numbers,
+    addNumber,
+  };
 }
-
-
-
